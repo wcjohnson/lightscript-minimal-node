f2c09cc83bcfe98cb2437d1164e866322ccf6bf1
William Johnson <wcjohnson@oigroup.net>
2018-09-24 22:05:21 -0700

    fix: Update `.eslintrc` in `test` dir

---
+++ test/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "parser": "@lightscript/eslint-plugin",
+  "plugins": ["@lightscript/eslint-plugin"],
+  "extends": [
+    "eslint:recommended",
+    "plugin:@lightscript/recommended"
+  ],
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "env": {
+    "node": true,
+    "es6": true,
+    "jest": true
+  },
+  "rules": {
+    "no-debugger": 0
+  }
+}
