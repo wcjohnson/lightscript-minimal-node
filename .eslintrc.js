4b0ac3f7b3ef6458b714c2e1cdcd7291226ab2a9
wcjohnson <wcjohnson@oigroup.net>
2020-07-16 18:11:11 -0700

    fix: Update linter recommended settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   "parser": "@lightscript/eslint-plugin",
   "plugins": ["@lightscript/eslint-plugin"],
   "extends": [
-    "eslint:recommended",
     "plugin:@lightscript/recommended"
   ],
   "parserOptions": {
